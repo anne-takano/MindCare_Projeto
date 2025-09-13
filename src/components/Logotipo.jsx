466ff854e001729a47d4c20886408f085430cf5f
Anne Takano <anne.takano@al.infnet.edu.br>
2025-09-13 18:55:06 +0000

    feat: Adiciona componente Logotipo
    Cria o componente visual para a exibição do logotipo da MindCare.
    
    - Inclui a estrutura JSX em `Logotipo.jsx`.
    - Adiciona estilos dedicados em `logotipo.estilos.css`.

---
+++ src/components/Logotipo.jsx
@@ -0,0 +1,6 @@
+import logoMindCare from "../img/logo.png";
+import "./logo.estilos.css";
+
+export default function Logotipo() {
+  return <img className="logo" src={logoMindCare} alt="Logo da MindCare" />;
+}
