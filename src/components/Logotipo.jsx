443520a204684a040d08271a28adf3d75159411a
Anne Takano <anne.takano@al.infnet.edu.br>
2025-09-13 19:09:20 +0000

    fix(Logotipo): Corrige caminho de importação do CSS

--- src/components/Logotipo.jsx
+++ src/components/Logotipo.jsx
@@ -1,5 +1,5 @@
 import logoMindCare from "../img/logo.png";
-import "./logo.estilos.css";
+import "./logotipo.estilos.css";
 
 export default function Logotipo() {
   return <img className="logo" src={logoMindCare} alt="Logo da MindCare" />;
